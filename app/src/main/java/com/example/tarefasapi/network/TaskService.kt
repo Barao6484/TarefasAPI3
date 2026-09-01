a255de9cc94ecd666bbced0cd4553ebbb9ff8578
Ronaldo Mango <rom31318@gmail.com>
2026-09-01 23:32:51 +0100

    feat: adicionar endpoint POST para criar tarefas

--- app/src/main/java/com/example/tarefasapi/network/TaskService.kt
+++ app/src/main/java/com/example/tarefasapi/network/TaskService.kt
@@ -1,13 +1,30 @@
 package com.example.tarefasapi.network
+
+import com.example.tarefasapi.model.CreateTaskRequest
 import com.example.tarefasapi.model.Task
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+
 /**
- * Interface responsável pelas chamadas a API
+ * Interface responsável pelas chamadas à API.
  */
 interface TaskService {
-    //edpoint "endereço" da api
+
+    /**
+     * Obtém a lista de tarefas da API.
+     */
     @GET("todos")
-    // devolve lista de tarefas
     fun listTasks(): Call<List<Task>>
+
+    /**
+     * Cria uma nova tarefa através de um pedido POST.
+     *
+     * @param task dados da nova tarefa enviados no corpo do pedido.
+     */
+    @POST("todos")
+    fun createTask(
+        @Body task: CreateTaskRequest
+    ): Call<Task>
 }
